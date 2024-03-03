9ee714d97ecac22c636fc11e7ea3d60e780721a3
Puddi <undisclosedpuddi@outlook.com>
2024-03-03 12:25:27 +0100

    merge sort

---
+++ merge_sort.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# Merge sort is a recursive algorithm that continuously splits the array
+# in half until it cannot be further divided i.e., the array has only one
+# element left (an array with one element is always sorted). Then the sorted
+# subarrays are merged into one sorted array.
+
+# Time Complexity: O(n*log(n))
+# It always divides the array into two halves and takes linear time to
+# merge two halves.
+
+# Space Complexity: O(n)
+# All elements are stored in R and L arrays
+
+
+A = input("Enter numbers separated by spaces: ")
+A = A.split()
+A = [int(n) for n in A]
+
+
+def merge_sort(A):
+    if len(A) > 1:
+        pivot = len(A) // 2
+
+        R = A[pivot:]
+        L = A[:pivot]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                A[k] = L[i]
+                i += 1
+            else:
+                A[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            A[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            A[k] = R[j]
+            j += 1
+            k += 1
+
+    return A
+
+
+# Runs
+print(merge_sort(A))
